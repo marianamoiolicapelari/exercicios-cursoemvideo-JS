e90b04129319b4b8b32e581307e767d4aa13ae53
marianamoiolicapelari <marianamoioli@gmail.com>
2022-05-05 15:25:58 -0300

    added new exercises

--- aula 12 ex/ex014/script.js
+++ aula 12 ex/ex014/script.js
@@ -1,17 +1,28 @@
 function carregar() {
+  var ola = document.getElementById('ola')
   var msg = document.getElementById('msg')
   var img = document.getElementById('imagem')
   var data = new Date()
   var hora = data.getHours()
-  msg.innerHTML = `Agora são ${hora} horas.`
-  if (hora >= 0 && hora < 12) {
-    // BOM DIA!
+  var min = data.getMinutes()
+  
+  msg.innerHTML = `Agora são ${hora}:${min}`
+  if (hora >= 6 && hora < 12) {
+    ola.innerHTML = `Bom dia!`
     img.src = 'manhã.png'
+    document.body.style.background = '#907A48'
   } else if (hora >= 12 && hora <= 18) {
-    // BOA TARDE!
+    ola.innerHTML = `Boa tarde!`
     img.src = 'tarde.png'
-  } else {
-    // BOA NOITE!
+    document.body.style.background = '#356CC9'
+  } else if (hora >= 19 && hora < 24) {
+    ola.innerHTML = `Boa noite!`
     img.src = 'noite.png'
+    document.body.style.background = '#12292F'
+  } else {
+    ola.innerHTML = `Boa madrugada!`
+    img.src = 'madrugada.png'
+    document.body.style.background = '#284888'
+
   }
 }
