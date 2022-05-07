2c2bac788fc352957d74640e31338cb73ff31db5
marianamoiolicapelari <marianamoioli@gmail.com>
2022-05-07 20:53:45 -0300

    Added new exercises

--- aula13/ambiente.js
+++ aula13/ambiente.js
@@ -2,22 +2,23 @@ var c = 1
 do {
   console.log(`Passo ${c}`)
   c++
-} while (c <= 6)
+} while (c <= 7)
 
 
-/*
+
 var c = 1
-while (c <= 6) {
+while (c <= 7) {
   console.log(`Passo ${c}`)
   c++
 }
-*/
+
+
 
 /*
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
-console.log('Tudo bem?')
+console.log('Passo 1')
+console.log('Passo 2')
+console.log('Passo 3')
+console.log('Passo 4')
+console.log('Passo 5')
+console.log('Passo 6')
 */
